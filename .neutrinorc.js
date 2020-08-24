fcae597cedcedb2d196639c7c9a220f1deccf021
Jan Werremeyer <jan.werremeyer@greenbone.net>
2020-08-24 12:20:15 +0200

    Add library framework

---
+++ .neutrinorc.js
@@ -0,0 +1,12 @@
+const reactComponents = require('@neutrinojs/react-components');
+const jest = require('@neutrinojs/jest');
+
+module.exports = {
+  options: {
+    root: __dirname,
+  },
+  use: [
+    reactComponents(),
+    jest(),
+  ],
+};
