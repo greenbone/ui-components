30c65be64c728af0a607ee3c26d2932fce22bf84
Jan Werremeyer <jan.werremeyer@greenbone.net>
2020-08-26 16:48:18 +0200

    Update ci integration

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ const neutrino = require('neutrino');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
-module.exports = neutrino().jest();
+module.exports = {
+    ...neutrino().jest(),
+    setupFilesAfterEnv: ["./jestSetup.js"],
+    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    testRegex: undefined
+};
