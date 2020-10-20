627b4743917bfae8b2b59108f3c976d0e6c7f715
Jan Werremeyer <jan.werremeyer@greenbone.net>
2020-10-20 17:25:10 +0200

    Remove old typefile

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Input } from "./Input";
-export { Flex, FlexColumn, FlexRow } from "./Flex";
-export { ColorTheme, Theme } from "./Theme";
-export { Headline, Bold, Caption, Paragraph, Subheadline, Text, Tiny } from "./Font";
-export { Button, AbortButton } from "./Button";
