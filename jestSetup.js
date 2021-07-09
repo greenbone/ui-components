04ccdc23adb8114e3966abe14e3959c58ce6c31b
Steffen Waterkamp <swaterkamp@gmx.de>
2021-07-09 12:44:16 +0200

    Migrate to create-react-app to manage project

--- jestSetup.js
+++ jestSetup.js
@@ -1,1 +1,7 @@
+import {configure} from "enzyme";
 import '@testing-library/jest-dom'
+import Adapter from "enzyme-adapter-react-16";
+import "@babel/polyfill";
+
+
+configure({adapter: new Adapter()});
