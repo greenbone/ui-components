5464aa709ee4d8e21e75b7b5f7402685053b6b35
Jan Werremeyer <jan.werremeyer@greenbone.net>
2021-07-19 13:25:09 +0200

    Make sure tests are passing

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,19 @@
 module.exports = {
     roots: ["src"],
-    setupFilesAfterEnv: ["./jest.setup.ts"],
-    moduleFileExtensions: ["ts", "tsx", "js"],
+    setupFilesAfterEnv: ["./jest.setup.js"],
+    moduleFileExtensions: ["js","jsx","ts", "tsx"],
     testPathIgnorePatterns: ["node_modules/"],
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.jsx?$": "babel-jest"
     },
-    testMatch: ["**/*.test.(ts|tsx)"],
+    testMatch: ["**/*.test.(ts|tsx|js|jsx)"],
     moduleNameMapper: {
         // Mocks out all these file formats when tests are run.
         "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "identity-obj-proxy",
         "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    }
+    },
+    moduleDirectories: ["node_modules", "utils"],
+    testEnvironment: "jsdom"
 };
--- utils/test-utils.js
+++ utils/test-utils.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {render} from "@testing-library/react"
 import {ThemeProvider} from "styled-components"
-import {Flex} from "../src/components/library/flex"
 import {ColorTheme} from "../theme"
 
 
